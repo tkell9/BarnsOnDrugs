9461e3971243032f7d76ad43eb9f94ffdbda0503
tkell9 <tkell9@hotmail.com>
2017-10-08 19:46:06 +1000

    v0.03 Upload from Bean Burrito.

--- Assets/1Scripts/playerController.cs
+++ Assets/1Scripts/playerController.cs
@@ -9,11 +9,9 @@ public class playerController : MonoBehaviour {
     //private MeshRenderer mr;
 
     private bool scored;
-    private int randomNum1;
-    private int randomNum2;
-    private int randomNum3;
-    private string[] GamePadControls = {"Abutton", "BButton", "XButton", "YButton", "LBumper", "RBumper", "LTrigger", "RTrigger", "DpadUp", "DpadDown", "DpadLeft", "DpadRight", "BackButton", "StartButton", "LJoystickVertical", "LJoystickHorizontal", "RJoystickVertical", "RJoystickHorizontal", "LJoystickButton", "RJoystickButton", };
-    private string[] chosenControls = { };
+    private string[] GamePadControls = {"AButton", "BButton", "XButton", "YButton", "LBumper", "RBumper", "LTrigger", "RTrigger", "DpadUp", "DpadDown", "DpadLeft", "DpadRight", "BackButton", "StartButton", "LJoystickVertical", "LJoystickHorizontal", "RJoystickVertical", "RJoystickHorizontal", "LJoystickButton", "RJoystickButton", };
+    private string[] GamePadControlsCopy = { "AButton", "BButton", "XButton", "YButton", "LBumper", "RBumper", "LTrigger", "RTrigger", "DpadUp", "DpadDown", "DpadLeft", "DpadRight", "BackButton", "StartButton", "LJoystickVertical", "LJoystickHorizontal", "RJoystickVertical", "RJoystickHorizontal", "LJoystickButton", "RJoystickButton", };
+    private string[] cuurentControls = { };
 
     //Keycodes for the three controls so they can be changed in a function below
     public float speed = 5f;
@@ -21,19 +19,23 @@ public class playerController : MonoBehaviour {
     public string kcDown; //= KeyCode.DownArrow;
     public string kcShoot; //= KeyCode.Space;
 
-    
+    private int randomNum1;
+    private int randomNum2;
+    private int randomNum3;
+        
 
-    //this is for setting the keybode later.
+
+    //this is for setting the keycode later.
     //kcFordward = KeyCode.W;
 
-    // Use this for initialization
+    
     void Start ()
     {
         //String array for every gamepad input. Will return a random item from array to set as keycode.
         //GamePadControls =
 
         changeControls();
-        
+
     }
 
     private void Update()
@@ -43,39 +45,67 @@ public class playerController : MonoBehaviour {
             transform.position += Vector3.up * speed * Time.deltaTime;
         }
 
-        if (Input.GetAxis(kcUp) > 0)
+        if (Input.GetAxis(kcUp) > 0.1f)
         {
             transform.position += Vector3.up * speed * Time.deltaTime;
+            
         }
 
         if (Input.GetButton(kcDown) == true)
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            transform.position -= Vector3.up * speed * Time.deltaTime;
         }
 
-        if (Input.GetAxis(kcDown) > 0)
+        if (Input.GetAxis(kcDown) > 0.1f)
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            transform.position -= Vector3.up * speed * Time.deltaTime;
         }
 
         if (Input.GetButton(kcShoot) == true)
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            
         }
 
         if (Input.GetAxis(kcShoot) > 0)
         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
+            
         }
 
     }
 
-    /*private void randomizeControls()
+    private void randomizeNumbers()
     {
-        //kcUp = KeyCode.JoystickButton0;
         int randomNum1 = Random.Range(0, 22);
         int randomNum2 = Random.Range(0, 22);
         int randomNum3 = Random.Range(0, 22); 
+    } 
+
+
+    /*public int[] GenerateUniqueRandom(int amount, int min, int max)
+    {
+        int[] arr = new int[amount];
+        for (int index = 0; index < amount; index++)
+        {
+            bool done = false;
+            while (!done)
+            {
+                int num = Random.Range(min, max);
+                int jindex = 0;
+                for (jindex = 0; jindex < index; jindex++)
+                {
+                    if (num == arr[jindex])
+                    {
+                        break;
+                    }
+                }
+                if (jindex == index)
+                {
+                    arr[index] = num;
+                    done = true;
+                }
+            }
+        }
+        return arr;
     } */
 
     private void changeControls()
@@ -83,12 +113,73 @@ public class playerController : MonoBehaviour {
         {
             if (this)
             {
-                kcUp = GetRandomStringItem();
-                kcDown = GetRandomStringItem();
-                kcShoot = GetRandomStringItem();
+                //kcUp = GamePadControls[randomNum1];
+
+                string NewUp = GamePadControls[randomNum1];
+                GamePadControls[randomNum1].Remove(randomNum1);
+
+                kcUp = NewUp;
+
+                Debug.Log(NewUp);
+
+
+                string NewDown = GamePadControls[randomNum1];
+                GamePadControls[randomNum1].Remove(randomNum1);
+
+                kcDown = NewDown;
+
+                Debug.Log(NewDown);
+
+
+                string NewShoot = GamePadControls[randomNum1];
+                GamePadControls[randomNum1].Remove(randomNum1);
+
+                kcShoot = NewShoot;
+
+                Debug.Log(NewShoot);
+
+                //kcUp = GetRandomStringItem();
+
+                //kcDown = GetRandomStringItem();
+
+                //kcShoot = GetRandomStringItem();
+
+                
+                
+
             }
         }
     }
+
+    void changeControls2()
+    {
+        kcUp = GetRandomStringItem();
+
+        kcDown = GetRandomStringItem();
+
+        kcShoot = GetRandomStringItem();
+
+        if (kcUp == kcDown)
+        {
+            kcUp = GetRandomStringItem();
+        }
+        else { kcUp = GetRandomStringItem(); }
+
+
+        if (kcDown == kcShoot)
+        {
+            kcDown = GetRandomStringItem();
+        }
+        else { kcDown = GetRandomStringItem(); }
+
+
+        if (kcShoot == kcUp)
+        {
+            kcShoot = GetRandomStringItem();
+        }
+        else { kcShoot = GetRandomStringItem(); }
+    }
+
 
     public string GetRandomStringItem()
     {
